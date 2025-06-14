b99b33c1b667ba5f509e8b8d7524e68c6460b570
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-14 22:24:30 +0530

    feat: inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  weight: '100', // Thin weight
+  subsets: ['latin'],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
