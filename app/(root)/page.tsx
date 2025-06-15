7234108ea8677bbc5a3496172f30202200bbab67
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-15 12:24:16 +0530

    feat: border

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Home from '../home/page'
-import Models from '../models/page'
-import Offer from '../offer/page'
+import React from "react";
+import Home from "../home/page";
+import Models from "../models/page";
+import Offer from "../offer/page";
 
 const page = () => {
   return (
-    <div className='mb-50'>
-        <Home/>
-        <Models/>
-        <Offer/>
+    <div className="mb-50">
+      <Home />
+      <Models />
+      <Offer />
+      <div className="mt-30 text-gray-300">
+        <hr />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
