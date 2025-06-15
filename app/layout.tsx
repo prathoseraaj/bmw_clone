5be92b698e8fb496a0c21ccf014232ddcebf8224
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-15 13:53:09 +0530

    feat: nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import Image from "next/image";
+import { CgProfile } from "react-icons/cg";
+import { IoCartOutline } from "react-icons/io5";
+import { CiHeart, CiLocationOn } from "react-icons/ci";
 
 const inter = Inter({
-  weight: '100', // Thin weight
-  subsets: ['latin'],
+  weight: "100", // Thin weight
+  subsets: ["latin"],
 });
 
 const geistSans = Geist({
@@ -34,10 +38,49 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="absolute z-50 top-1  pl-30 pr-30 pt-3">
+          <nav className="flex justify-between">
+            <div className="flex flex-row gap-7">
+              <Image
+                src={
+                  "https://www.bmw.in/content/dam/bmw/common/images/logo-icons/BMW/BMW_White_Logo.svg.asset.1670245093434.svg"
+                }
+                alt="bmw logo"
+                height={50}
+                width={50}
+              />
+              <h1 className="mt-3 text-white text-[15px] font-semibold">
+                Models
+              </h1>
+              <h1 className="mt-3 text-white text-[15px] font-semibold">
+                Electric
+              </h1>
+              <h1 className="mt-3 text-white text-[15px] font-semibold">
+                Configurator
+              </h1>
+              <h1 className="mt-3 text-white text-[15px] font-semibold">
+                Visit Online Shop
+              </h1>
+              <h1 className="mt-3 text-white text-[15px] font-semibold">
+                More BMW
+              </h1>
+            </div>
+            <div className="flex flex-row gap-6 text-white text-[20px]">
+              <CgProfile className="ml-80 mt-3" />
+              <IoCartOutline className=" mt-3" />
+              <CiHeart className="mt-3" />
+              <CiLocationOn className="mt-3" />
+            </div>
+          </nav>
+          <div className="text-white mt-3 w-full">
+            <hr />
+          </div>
+        </div>
+
         <main>{children}</main>
-      <footer className="">
-        <Footer/>
-      </footer>
+        <footer className="">
+          <Footer />
+        </footer>
       </body>
     </html>
   );
