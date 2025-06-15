7f57f9d6a4f7beb98cb1d4873ae4c89322ff6881
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-15 13:05:16 +0530

    feat: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from 'next/font/google';
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   weight: '100', // Thin weight
@@ -33,7 +34,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>{children}</main>
+      <footer className="">
+        <Footer/>
+      </footer>
       </body>
     </html>
   );
